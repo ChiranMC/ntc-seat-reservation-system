7042ec6eda768bc3add79d6546e19e46ab92f67f
ChiranMC <wilegoda1234@gmail.com>
2024-12-11 23:06:16 +0530

    Update app.js
    DB troubleshoot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { testConnection } = require(".src/config/database.js");
+const { testConnection } = require("./src/config/database");
 const app = express();
 const port = process.env.PORT || 3001;
 
