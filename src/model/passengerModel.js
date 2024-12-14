4e9a3c5140e643b5bfb17cbf01ee4715ddccdff4
ChiranMC <wilegoda1234@gmail.com>
2024-12-14 15:10:34 +0530

    error 👍  : user registry fixed

--- src/model/passengerModel.js
+++ src/model/passengerModel.js
@@ -1,11 +1,11 @@
 class Passanger {
     constructor(nic_no, full_name, address, contact_number, email, password){
-        this.nic_no;
-        this.full_name;
-        this.address;
-        this.contact_number;
-        this.email;
-        this.password;
+        this.nic_no = nic_no;
+        this.full_name = full_name;
+        this.address = address;
+        this.contact_number = contact_number;
+        this.email = email;
+        this.password = password;
     }
 }
 module.exports = Passanger;
