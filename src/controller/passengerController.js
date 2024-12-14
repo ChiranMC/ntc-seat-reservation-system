240e413d44404c0900a93b15ef83866b249bae02
ChiranMC <wilegoda1234@gmail.com>
2024-12-14 15:24:15 +0530

    fix 2

--- src/controller/passengerController.js
+++ src/controller/passengerController.js
@@ -4,7 +4,8 @@ class PassengerController {
   async registerPassenger(req, res) {
     try {
       const { nic_no, full_name, address, contact_number, email, password } = req.body;
-      const newPassenger = await passengerService.registerPassenger(nic_no, full_name, address, contact_number, email, password);
+      const formatedContactNo = parseInt(contact_number, 10);
+      const newPassenger = await passengerService.registerPassenger(nic_no, full_name, address, formatedContactNo, email, password);
       res.status(201).json({ message: 'Passenger registered successfully', newPassenger: newPassenger.nic_no });
       console.log('new passenger added to the system passenger :', newPassenger.full_name, newPassenger.nic_no);
     } catch (error) {
