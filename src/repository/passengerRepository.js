f7916eaaae181e9ce39544335cdfe8b742bde4a7
ChiranMC <wilegoda1234@gmail.com>
2024-12-15 23:54:32 +0530

    initial commit for the login code

--- src/repository/passengerRepository.js
+++ src/repository/passengerRepository.js
@@ -5,6 +5,10 @@ class PassengerRepository {
     const newPassenger = await Passenger.create(passenger);
     return newPassenger;
   }
+
+  async findByNic(nic_no){
+    return await Passenger.findOne({where: { nic_no }})
+  }
 }
 
 module.exports = new PassengerRepository();
--- src/service/passengerService.js
+++ src/service/passengerService.js
@@ -9,6 +9,22 @@ class PassengerService {
     const createPassenger = await PassangerRepository.createPassenger(passenger);
     return createPassenger;
   }
+
+  async verifyPassenger(nic_no, password){
+    const passengerDetails = await PassangerRepository.findByNic(nic_no);
+    if (!passengerDetails) {
+      throw new Error('Invalid nic');
+    }
+    else{
+      const verifyPassword = await bcrypt.compare(password, passengerDetails.password);
+      if (verifyPassword) {
+        return passengerDetails;
+      }
+      else{
+        throw new Error('invalid password');
+      }
+    }
+  }
 }
 
 module.exports = new PassengerService();
