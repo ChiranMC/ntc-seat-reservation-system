8d4df2455ad97f9ac2ade4c09f91de183d898334
ChiranMC <wilegoda1234@gmail.com>
2024-12-28 23:35:19 +0530

    model bug fix : pyemnt rcpt

--- src/model/paymentRecieptHistory.js
+++ src/model/paymentRecieptHistory.js
@@ -2,10 +2,9 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const PaymentRecieptHistory = sequelize.define('PaymentRecieptHistory',{
-    payment_reciept_id:{
+    payment_receipt_id:{
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrementIdentity: true
     },
     passenger_id: DataTypes.STRING,
     payment_amount: DataTypes.FLOAT,
