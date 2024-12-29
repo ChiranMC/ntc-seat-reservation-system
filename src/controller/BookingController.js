d08ed868b05e363d312bf9fb380b4c05fe268168
ChiranMC <wilegoda1234@gmail.com>
2024-12-29 12:10:44 +0530

    log update ->

--- src/controller/BookingController.js
+++ src/controller/BookingController.js
@@ -10,6 +10,10 @@ class BookingController{
                 res.status(200).json(`Successfully saved booking information for ${status}`);
                 console.log("successfully saved booking info");
             }
+            else if(status === 0){
+                res.status(500).json(`failed to save booking information`);
+                console.log('failed to save booking information');
+            }
         } catch (error) {
             res.status(500).json("failed to save booking information");
             console.log(`error occured while calling bus ticket service method to save and create bookings ${error}`)
