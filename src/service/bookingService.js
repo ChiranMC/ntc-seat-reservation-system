d258eac0fc7195f66d58d76dbe371cc075722218
ChiranMC <wilegoda1234@gmail.com>
2024-12-29 13:29:46 +0530

    Update bookingService.js

--- src/service/bookingService.js
+++ src/service/bookingService.js
@@ -10,7 +10,9 @@ class BookingService {
 
     async getAllAvailableSeats(number_plate, scheduled_slot, booking_date){
         try {
+            console.log("request came to fetch seats availabilty information")
             const busType = BusesRepository.getBusTypeByNumberPlate(number_plate);
+            console.log(`bus type retreived ${busType}`);
             if (busType === 'Semi-Luxury' || busType === 'Normal') {
                 const bookedSeats = await PassengerBookingsRepository.getBookedSeatsListByNumberPlateAndTimeSlot(number_plate, scheduled_slot, booking_date);
                 const availableSeats = bustSeatsUtill.filterAvailableSemiLuxuryOrNormalSeats(bookedSeats);
